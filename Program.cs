3e548bc91ca921e6646c498dab94af69d9b9579d
matticry <mateoeras07@gmail.com>
2023-11-03 16:40:52 -0500

    agenda_1.0

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using MVC.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+// Agregar esta linea de codigo para la conexion
+builder.Services.AddSingleton(new DbContext(builder.Configuration.GetConnectionString("conexion")!));
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
